65134b9b2873cb78260dab2b40c5fdfddd60ea70
Nhan Vo Thanh <nhanvothanh719@gmail.com>
2022-10-02 13:01:11 +0700

    Use Glide to display image with validating URL

---
+++ app/src/main/java/com/example/imageapp/DatabaseHelper.java
@@ -0,0 +1,64 @@
+package com.example.imageapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private Context context;
+
+    public static final String DATABASE_NAME = "imageURL.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_NAME = "image_URLs";
+    //Columns of the table
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_ADDRESS = "address";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String create_table_query = "CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_ADDRESS + " TEXT NOT NULL);";
+        sqLiteDatabase.execSQL(create_table_query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public void addImage(String address) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_ADDRESS, address);
+        long result = database.insert(TABLE_NAME, null, contentValues);
+        if(result == -1) {
+            Toast.makeText(context, "Fail to add image URL", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(context, "Add image successfully", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public Cursor getAllImages() {
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase database = this.getReadableDatabase();
+        Cursor cursor = null;
+        if(database != null) {
+            cursor = database.rawQuery(query, null);
+        }
+        return  cursor;
+    }
+}
--- app/src/main/java/com/example/imageapp/MainActivity.java
+++ app/src/main/java/com/example/imageapp/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,11 +24,8 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageViewImage;
-    Button buttonBackward, buttonForward, buttonAddLink;
+    Button buttonBackward, buttonForward, buttonAddImageURL;
     EditText editTextImageURL;
-
-    Handler handler = new Handler();
-    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,57 +38,41 @@ public class MainActivity extends AppCompatActivity {
 
         buttonBackward = (Button) findViewById(R.id.buttonBackward);
         buttonForward = (Button) findViewById(R.id.buttonForward);
-        buttonAddLink = (Button) findViewById(R.id.buttonAddLink);
+        buttonAddImageURL = (Button) findViewById(R.id.buttonAddImageURL);
 
         buttonBackward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Get URL from input
                 String imageURL = editTextImageURL.getText().toString();
-                new FetchImage(imageURL).start();
+                Glide.with(getApplicationContext())
+                        .load(imageURL)
+                        .placeholder(R.drawable.ic_image_search)
+                        .error(R.drawable.ic_broken_image)
+                        .centerCrop()
+                        .override(320, 320)
+                        .into(imageViewImage);
             }
         });
 
+        buttonAddImageURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
+
+                String inputImageURL = editTextImageURL.getText().toString().trim();
+
+                if(checkImageURLExist(inputImageURL) == false) {
+                    Toast.makeText(MainActivity.this, "Fail to add image URL", Toast.LENGTH_LONG).show();
+                } else {
+                    databaseHelper.addImage(inputImageURL);
+                    Toast.makeText(MainActivity.this, "Add image URL successfully", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
-    class FetchImage extends Thread {
-
-        String URL;
-        Bitmap bitmap;
-
-        FetchImage(String URL) {
-            this.URL = URL;
-        }
-
-        @Override
-        public void run() {
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    //Show progress dialog box
-                    progressDialog = new ProgressDialog(MainActivity.this);
-                    progressDialog.setMessage("Getting picture from Internet...");
-                    progressDialog.setCancelable(false);
-                    progressDialog.show();
-                }
-            });
-            InputStream inputStream = null;
-            try {
-                inputStream = new URL(URL).openStream();
-                bitmap = BitmapFactory.decodeStream(inputStream);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    if(progressDialog.isShowing()) {
-                        progressDialog.dismiss();
-                    }
-                    imageViewImage.setImageBitmap(bitmap);
-                }
-            });
-        }
+    private boolean checkImageURLExist(String inputImageURL) {
+        return URLUtil.isValidUrl(inputImageURL);
     }
 }
